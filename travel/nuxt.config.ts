65b8a18b36587cf2cb325663427b3b6098a0745b
SanchezZarm <sanchezzarm891@gmail.com>
2026-06-17 19:18:52 +0800

    first commit

---
+++ travel/nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'TravelPulse - Поиск выгодных путешествий',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'images/logo.png' }
+      ]
+    }
+  }
+})
