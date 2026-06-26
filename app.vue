f82beb045d452a0a894c8f783f88df01f954ad21
SanchezZarm <sanchezzarm891@gmail.com>
2026-06-27 00:38:54 +0800

    Fix

--- app.vue
+++ app.vue
@@ -13,13 +13,5 @@
 <script setup>
 import { ref } from 'vue'
 
-import AppHeader from '../components/AppHeader.vue'
-import AppFooter from '../components/AppFooter.vue'
-import SearchForm from '../components/SearchForm.vue'
-import PopularDestinations from '../components/PopularDestinations.vue'
-import PopularTours from '../components/PopularTours.vue'
-import PlanTrip from '../components/PlanTrip.vue'
-import LoginModal from '../components/LoginModal.vue'
-
 const showLogin = ref(false)
 </script>
