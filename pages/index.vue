21dcf8969c8e21d87333cda73ef6e7b0a09fd290
SanchezZarm <sanchezzarm891@gmail.com>
2026-06-27 00:37:01 +0800

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
-    <AppHeader @open-login="showLogin = true" />
-    <main style="padding-top: 20px;">
-      <PopularDestinations />
-      <PopularTours />
-      <PlanTrip />
-    </main>
-    <AppFooter @open-login="showLogin = true" />
+    <AppHeader @openLogin="showLogin = true" />
+    <SearchForm />
+    <PopularDestinations />
+    <PopularTours />
+    <PlanTrip />
+    <AppFooter />
     <LoginModal v-model="showLogin" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import AppHeader from '@/components/AppHeader.vue'
-import AppFooter from '@/components/AppFooter.vue'
-import SearchForm from '@/components/SearchForm.vue'
-import PopularDestinations from '@/components/PopularDestinations.vue'
-import PopularTours from '@/components/PopularTours.vue'
-import PlanTrip from '@/components/PlanTrip.vue'
-import LoginModal from '@/components/LoginModal.vue'
+
+import AppHeader from '../components/AppHeader.vue'
+import AppFooter from '../components/AppFooter.vue'
+import SearchForm from '../components/SearchForm.vue'
+import PopularDestinations from '../components/PopularDestinations.vue'
+import PopularTours from '../components/PopularTours.vue'
+import PlanTrip from '../components/PlanTrip.vue'
+import LoginModal from '../components/LoginModal.vue'
 
 const showLogin = ref(false)
 </script>
