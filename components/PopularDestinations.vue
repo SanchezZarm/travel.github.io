0c942b44c36e231f4fa66ad6a45be280ac8075d0
SanchezZarm <sanchezzarm891@gmail.com>
2026-06-27 01:56:11 +0800

    Fix

--- components/PopularDestinations.vue
+++ components/PopularDestinations.vue
@@ -38,7 +38,7 @@ const { t } = useI18n()
 const track = ref(null)
 
 const destinations = computed(() => [
-  { name: t('paris'), image: '`/travel.github.io/images/paris.png`' },
+  { name: t('paris'), image: '/travel.github.io/images/paris.png' },
   { name: t('rome'), image: '/travel.github.io/images/rim.png' },
   { name: t('bali'), image: '/travel.github.io/images/bali.png' },
   { name: t('baikal'), image: '/travel.github.io/images/baikal.png' }
