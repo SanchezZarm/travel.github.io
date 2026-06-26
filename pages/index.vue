df7b6908558fb6326f2d61f577eca0e8d94ea3a0
SanchezZarm <sanchezzarm891@gmail.com>
2026-06-27 00:28:14 +0800

    Fix

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppHeader @openLogin="showLogin = true" />
+    <SearchForm />
+    <PopularDestinations />
+    <PopularTours />
+    <PlanTrip />
+    <AppFooter />
+    <LoginModal v-model="showLogin" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AppHeader from '~/components/AppHeader.vue'
+import AppFooter from '~/components/AppFooter.vue'
+import SearchForm from '~/components/SearchForm.vue'
+import PopularDestinations from '~/components/PopularDestinations.vue'
+import PopularTours from '~/components/PopularTours.vue'
+import PlanTrip from '~/components/PlanTrip.vue'
+import LoginModal from '~/components/LoginModal.vue'
+
+const showLogin = ref(false)
+</script>
